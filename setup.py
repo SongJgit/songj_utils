197ae312025aa94871af4eec4c0ef2a382488362
songj <songj95@outlook.com>
2022-03-29 23:09:24 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="songj_utils",
+    version="1.0",
+    author="songj",
+    author_email="songj95@outlook.com",
+    packages=find_packages()
+)
